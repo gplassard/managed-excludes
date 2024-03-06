3ac6814fc23c83aba0bb81a96e3bd99047f3e84a
GitHub Action <action@github.com>
2024-03-06 23:44:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/gplassard/managedexcludes/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/gplassard/managedexcludes/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gplassard.managedexcludes.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/gplassard/managedexcludes/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/gplassard/managedexcludes/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.gplassard.managedexcludes.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.gplassard.managedexcludes.MyBundle
+import com.github.gplassard.managedexcludes.services.MyProjectService
 import javax.swing.JButton
 
 
