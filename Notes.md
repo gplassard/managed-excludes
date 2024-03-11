ca85773019a16e4a3c09ebd12c3dea2b5daff388
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2024-03-12 00:25:30 +0100

    action (#10)
    * feat(excludes): create action
    
    * feat(excludes): it works !

---
+++ Notes.md
@@ -0,0 +1,10 @@
+https://plugins.jetbrains.com/docs/intellij/welcome.html
+https://plugins.jetbrains.com/docs/intellij/basic-action-system.html
+https://github.com/JetBrains/intellij-community/blob/master/platform/lang-impl/src/com/intellij/ide/projectView/actions/UnmarkRootAction.java
+https://github.com/JetBrains/intellij-community/blob/master/platform/lang-impl/src/com/intellij/ide/projectView/actions/MarkRootActionBase.java
+https://github.com/JetBrains/intellij-community/blob/master/python/resources/META-INF/pycharm-core.xml#L49
+
+# UI
+* https://plugins.jetbrains.com/docs/intellij/user-interface-components.html
+* https://plugins.jetbrains.com/docs/intellij/kotlin-ui-dsl-version-2.html
+* https://jetbrains.design/intellij/
---
+++ src/main/kotlin/com/github/gplassard/managedexcludes/actions/AddManagedExcludesAction.kt
@@ -0,0 +1,35 @@
+package com.github.gplassard.managedexcludes.actions
+
+import com.github.gplassard.managedexcludes.services.ConfigService
+import com.github.gplassard.managedexcludes.services.ExcludeService
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformCoreDataKeys
+import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.thisLogger
+
+
+class AddManagedExcludesAction: AnAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+        thisLogger().info("START")
+        val module = event.getData(PlatformCoreDataKeys.MODULE)
+        if (module == null) {
+            thisLogger().warn("No module, aborting")
+            return
+        }
+        val excludeService = event.project?.service<ExcludeService>()
+        if (excludeService == null) {
+            thisLogger().warn("No exclude service, aborting")
+            return
+        }
+        val configService = event.project?.service<ConfigService>()
+        if (configService == null) {
+            thisLogger().warn("No config service, aborting")
+            return
+        }
+        val excludes = configService.loadExcludeConfig(module)
+        thisLogger().warn("Going to exclude ${excludes.size} files")
+        excludeService.excludePaths(module, excludes)
+        thisLogger().info("END")
+    }
+}
