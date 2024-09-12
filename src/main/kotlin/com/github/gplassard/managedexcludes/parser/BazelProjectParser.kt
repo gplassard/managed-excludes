fcd91a4455fc33d4aaa6562abeff39fb14e284ef
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2024-09-12 21:50:46 +0200

    chore(logs): additional logging

--- src/main/kotlin/com/github/gplassard/managedexcludes/parser/BazelProjectParser.kt
+++ src/main/kotlin/com/github/gplassard/managedexcludes/parser/BazelProjectParser.kt
@@ -1,5 +1,7 @@
 package com.github.gplassard.managedexcludes.parser
 
+import com.intellij.openapi.diagnostic.thisLogger
+
 object BazelProjectParser {
 
     fun parseExcludedDirectories(content: String): Set<String> {
@@ -9,7 +11,9 @@ object BazelProjectParser {
             .filterNot { it.trim().startsWith("#") }  // ignore all comment lines
             .dropWhile { it.trim() != "directories:" }      // Drop lines until "directories:" is found
             .drop(1)                                        // Skip the "directories:" line
+            .also { thisLogger().info("After directories ${it.joinToString()}") }
             .takeWhile { regex.matches(it) }                // Take lines that match the indentation pattern
+            .also { thisLogger().info("Directories entries ${it.joinToString()}") }
             .map { regex.matchEntire(it)!!.groupValues[1].trim() }
             .filter { it.startsWith("-") } // only keep lines that are using exclusions (starting with a -)
             .map { it.substring(1).trim() }
